1fbec3e3fc96e57f1c276cac12e9c0fa26dab35b
bumbeishvili <me@davidb.dev>
2022-07-14 11:50:06 +0400

    improve guide layout

--- src/routes/__layout-guide.svelte
+++ src/routes/__layout-guide.svelte
@@ -4,21 +4,44 @@
 </script>
 
 <header>
- <Title title="Guide Layout" />
+ <nav>
+  <Title title="Ninja Gaming Guides" />
+  <div class="links">
+   <a href="/">Home</a>
+   <a href="/about">About</a>
+   <a href="/guides">Guides</a>
+  </div>
+ </nav>
 </header>
 <main>
- <!-- Page Content-->
  <slot />
 </main>
-
+<footer>
+ <p>Copyright 2022 Ninja Gaming Guides</p>
+</footer>
 
 <style>
  header {
-  display: flex;
-  justify-content: center;
+  text-align: center;
+  background: rgba(0, 0, 0, 0.1);
  }
  main {
   max-width: 960px;
   margin: 20px auto;
+ }
+ footer {
+  text-align: center;
+ }
+ nav {
+  display: flex;
+  align-items: center;
+  max-width: 960px;
+  margin: 0 auto;
+ }
+ .links {
+  margin-left: auto;
+ }
+ a {
+  margin-left: 10px;
  }
 </style>
